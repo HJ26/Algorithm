6e53d0c82736463793135ecdc3fb5dc84af85d18
HJ26 <57403168+HJ26@users.noreply.github.com>
2023-10-12 17:18:20 +0900

    [Silver I] Title: 스타트와 링크, Time: 284 ms, Memory: 17200 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,93 +1,77 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.StringTokenizer;
- 
+
 public class Main {
-	
-	static int N;
-	static int[][] map;
+	static int N, minDiff;
+	static int[][ ] map;
+	static int[] tmp,tmp2;
 	static boolean[] visit;
-	
-	static int Min = Integer.MAX_VALUE;
-	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
+		StringTokenizer st;
 		N = Integer.parseInt(br.readLine());
- 
 		map = new int[N][N];
-		visit = new boolean[N];
- 
- 
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
- 
-			for (int j = 0; j < N; j++) {
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < N; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
- 
-		combi(0, 0);
-		System.out.println(Min);
- 
+		
+		// 팀을 나눕니다
+		minDiff = Integer.MAX_VALUE;
+		tmp = new int[N/2];
+		tmp2 = new int[N/2];
+		visit = new boolean[N];
+		divideTeam(0,0);
+
+		System.out.println(minDiff);
 	}
- 
-	// idx는 인덱스, count는 조합 개수(=재귀 깊이)
-	static void combi(int idx, int count) {
-		// 팀 조합이 완성될 경우
-		if(count == N / 2) {
-			/*
-			 방문한 팀과 방문하지 않은 팀을 각각 나누어
-			 각 팀의 점수를 구한 뒤 최솟값을 찾는다.
-			*/
-			diff();
+	
+	// 팀의 조합을 구하는 함수
+	private static void divideTeam(int idx, int sidx) {
+	
+		// idx = 사람번호
+		// sidx = 뽑은 사람 수
+		
+		if(sidx == N/2) {
+			for(int i = 0, idx2 = 0; i < N; i++) {
+				if(!visit[i]) {
+					tmp2[idx2++] = i;
+				}
+			}
+			findMin();
 			return;
 		}
- 
-		for(int i = idx; i < N; i++) {
-			// 방문하지 않았다면?
-			if(!visit[i]) {
-				visit[i] = true;	// 방문으로 변경
-				combi(i + 1, count + 1);	// 재귀 호출
-				visit[i] = false;	// 재귀가 끝나면 비방문으로 변경
+		
+		if(idx == N) return;
+		tmp[sidx] = idx;
+		visit[idx] = true;
+		divideTeam(idx+1, sidx+1);
+		visit[idx] = false;
+		divideTeam(idx+1, sidx);
+		
+	}
+
+	private static void findMin() {
+		
+		int team1 = 0;
+		int team2 = 0;
+		for(int i : tmp) {
+			for(int j : tmp) {
+				team1 += map[i][j];
 			}
 		}
-	}
- 
-	// 두 팀의 능력치 차이를 계산하는 함수 
-	static void diff() {
-		int team_start = 0;
-		int team_link = 0;
- 
-		for (int i = 0; i < N - 1; i++) {
-			for (int j = i + 1; j < N; j++) {
-				// i 번째 사람과 j 번째 사람이 true라면 스타트팀으로 점수 플러스 
-				if (visit[i] == true && visit[j] == true) {
-					team_start += map[i][j];
-					team_start += map[j][i];
-				}
-				// i 번째 사람과 j 번째 사람이 false라면 링크팀으로 점수 플러스 
-				else if (visit[i] == false && visit[j] == false) {
-					team_link += map[i][j];
-					team_link += map[j][i];
-				}
+		
+		for(int i : tmp2) {
+			for(int j : tmp2) {
+				team2 += map[i][j];
 			}
 		}
-		// 두 팀의 점수 차이 (절댓값)
-		int val = Math.abs(team_start - team_link);
 		
-		/*
-		  두 팀의 점수차가 0이라면 가장 낮은 최솟값이기 때문에
-		  더이상의 탐색 필요없이 0을 출력하고 종료하면 된다.
-		 */
-		if (val == 0) {
-			System.out.println(val);
-			System.exit(0);
-		}
+		minDiff = Math.min(minDiff, Math.abs(team1-team2)); 
 		
-		Min = Math.min(val, Min);
-				
 	}
- 
+	
+	
 }
