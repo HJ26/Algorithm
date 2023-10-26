55076579b7948da25789744913fd09ee4ba61db9
HJ26 <57403168+HJ26@users.noreply.github.com>
2023-10-26 20:22:46 +0900

    [Silver III] Title: N과 M (3), Time: 632 ms, Memory: 157336 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N,M;
+	static int[] nums;
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());		// 수 최대값
+		M = Integer.parseInt(st.nextToken());		// 조합 길이
+		nums = new int[M];							// 완성된 조합
+		combi(0);									// 만들기
+		bw.close();									// 출력
+		br.close();
+	}
+	private static void combi(int sidx) throws IOException {
+		if(sidx == M) {											// M개 다 뽑았으면
+			for(int i = 0; i < M; i++) {						// 값 순서대로 입력하고
+				bw.write(Integer.toString(nums[i]));
+				bw.write(" ");
+			}
+			bw.write("\n");										// 버퍼라이터에 저장
+			return;												// 함수 종료
+		}
+
+		for(int i = 1; i <= N; i++) {							// 조합 숫자 고르기
+			nums[sidx] = i;
+			combi(sidx+1);
+		}
+		
+	}
+}
