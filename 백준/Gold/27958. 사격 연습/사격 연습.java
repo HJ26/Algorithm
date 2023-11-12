087111d2d8c2d946ae64d59fefb2a012dc6fd714
HJ26 <57403168+HJ26@users.noreply.github.com>
2023-11-12 23:46:00 +0900

    [Gold IV] Title: 사격 연습, Time: 192 ms, Memory: 25192 KB -BaekjoonHub

--- 백준/Gold/27958. 사격 연습/사격 연습.java
+++ 백준/Gold/27958. 사격 연습/사격 연습.java
@@ -3,10 +3,11 @@ import java.util.*;
 
 public class Main {
 	static int N,K, maxScore = 0;
-	static int[] attack, dx = {-1, 1, 0, 0}, dy = {0,0,-1,1}, choice;
+	static int[] attack, dx = {-1, 1, 0, 0}, dy = {0,0,-1,1};
 	static int[][][] map;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
 		
 		N = Integer.parseInt(br.readLine());	// 지도 크기
@@ -31,63 +32,65 @@ public class Main {
 		
 		
 		// 쏘기
-		choice = new int[K];
-		combi(0);
+		shoot(0, 0, map);
 		System.out.println(maxScore);
 	}
-	private static void combi(int sidx) {
-		if(sidx == K) {
-			maxScore = Math.max(maxScore, shoot());
+	private static void shoot(int nAttack, int score, int[][][] map) {
+		
+		if(nAttack == K) {
+			maxScore = Math.max(maxScore, score);
 			return;
 		}
+		
+		// 지도 정보 복사
+		int[][][] mapTmp = new int[N][N][2];
 		for(int i = 0; i < N; i++) {
-			choice[sidx] = i;
-			combi(sidx+1);
-		}
-		
-	}
-	private static int shoot() {
-		
-		int mapTmp[][][] = new int[N][N][2];
-		int score = 0;
-		for(int k = 0; k < N; k++) {
-			for(int l = 0; l < N; l++) {
-				mapTmp[k][l][0] = map[k][l][0];
-				mapTmp[k][l][1] = map[k][l][1];
+			for(int j = 0; j < N; j++) {
+				mapTmp[i][j][0] = map[i][j][0];
+				mapTmp[i][j][1] = map[i][j][1];
 			}
 		}
 		
-		loop: for(int i = 0; i < K; i++) {
-			int row = choice[i];
+		// 쏘기
+		loop : for(int i = 0; i < N; i++) {
 			for(int j = 0; j < N; j++) {
-				if(mapTmp[row][j][0] != 0) {
-					if(mapTmp[row][j][0] >= 10) {
-						score += mapTmp[row][j][1];
-						mapTmp[row][j][0] = 0;
-						mapTmp[row][j][1] = 0;
+				if(mapTmp[i][j][0] != 0) {
+					if(mapTmp[i][j][0] >= 10) {
+						mapTmp[i][j][0] = 0;
+						shoot(nAttack+1,score+mapTmp[i][j][1],mapTmp);
 					}
-					else{
-						mapTmp[row][j][0] -= attack[i];
-						if(mapTmp[row][j][0] <= 0) {
-							mapTmp[row][j][0] = 0;
-							score += mapTmp[row][j][1];
-							if(mapTmp[row][j][1] > 3) {
-								
+					else {
+						mapTmp[i][j][0] -= attack[nAttack];
+						if(mapTmp[i][j][0] <= 0) {
+							mapTmp[i][j][0] = 0;
+							if(mapTmp[i][j][1] > 3) {
 								for(int dir = 0; dir < 4; dir++) {
-									int nx = row + dx[dir];
-									int ny = j + dy[dir];
-									if(nx < 0 || nx >= N || ny < 0 || ny >= N || mapTmp[nx][ny][0] != 0) continue;
-									mapTmp[nx][ny][1] = mapTmp[row][j][1] / 4;
-									mapTmp[nx][ny][0] = mapTmp[nx][ny][1];
-								}
+									int nextX = i + dx[dir];
+									int nextY = j + dy[dir];
+									if(nextX < 0 || nextX >= N || nextY < 0 || nextY >= N || mapTmp[nextX][nextY][0] != 0) continue;
+									else {
+										mapTmp[nextX][nextY][0] = mapTmp[i][j][1] / 4;
+										mapTmp[nextX][nextY][1] = mapTmp[nextX][nextY][0];
+									}
+								}								
 							}
+							shoot(nAttack+1, score + mapTmp[i][j][1], mapTmp);
+						}else {
+							shoot(nAttack+1, score, mapTmp);
 						}
 					}
-					break;
+					for(int k = 0; k < N; k++) {
+						for(int l = 0; l < N; l++) {
+							mapTmp[k][l][0] = map[k][l][0];
+							mapTmp[k][l][1] = map[k][l][1];
+						}
+					}
+					continue loop;
 				}
 			}
 		}
-		return score;
+		
+		
 		
 	}
 }
