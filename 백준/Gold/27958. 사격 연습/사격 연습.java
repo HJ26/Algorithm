443d5ea7d8ca4cb4fdc87fc78c4b4fa3436278e9
HJ26 <57403168+HJ26@users.noreply.github.com>
2023-11-12 23:50:47 +0900

    [Gold IV] Title: 사격 연습, Time: 184 ms, Memory: 25096 KB -BaekjoonHub

--- 백준/Gold/27958. 사격 연습/사격 연습.java
+++ 백준/Gold/27958. 사격 연습/사격 연습.java
@@ -7,7 +7,6 @@ public class Main {
 	static int[][][] map;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
 		
 		N = Integer.parseInt(br.readLine());	// 지도 크기
@@ -37,6 +36,7 @@ public class Main {
 	}
 	private static void shoot(int nAttack, int score, int[][][] map) {
 		
+		// K번 쐈으면 최대 점수 갱신
 		if(nAttack == K) {
 			maxScore = Math.max(maxScore, score);
 			return;
@@ -52,39 +52,44 @@ public class Main {
 		}
 		
 		// 쏘기
-		loop : for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				if(mapTmp[i][j][0] != 0) {
-					if(mapTmp[i][j][0] >= 10) {
-						mapTmp[i][j][0] = 0;
-						shoot(nAttack+1,score+mapTmp[i][j][1],mapTmp);
+		loop : for(int i = 0; i < N; i++) {		// 한줄씩 쏘기
+			for(int j = 0; j < N; j++) {		// 그 줄의 각 열 확인
+				if(mapTmp[i][j][0] != 0) {		// 0이 아니면
+					if(mapTmp[i][j][0] >= 10) {	// 10점 이상인 보너스 타겟일때
+						mapTmp[i][j][0] = 0;	// 0점으로 바꾸고
+						shoot(nAttack+1,score+mapTmp[i][j][1],mapTmp);	// 다음 쏘기
 					}
-					else {
-						mapTmp[i][j][0] -= attack[nAttack];
-						if(mapTmp[i][j][0] <= 0) {
-							mapTmp[i][j][0] = 0;
-							if(mapTmp[i][j][1] > 3) {
-								for(int dir = 0; dir < 4; dir++) {
+					else {	// 보너스 타겟이 아니면
+						mapTmp[i][j][0] -= attack[nAttack];		// 공격력만큼 감소시키기
+						if(mapTmp[i][j][0] <= 0) {				// 감소한 값이 0 이하면
+							mapTmp[i][j][0] = 0;				// 0으로 바꾸고
+							if(mapTmp[i][j][1] > 3) {			// 4점 이상일때 ( 상하좌우에 새로운 타겟을 만들 수 있을 때 )	
+								for(int dir = 0; dir < 4; dir++) {	// 사방탐색하면서
 									int nextX = i + dx[dir];
 									int nextY = j + dy[dir];
+									// 지도 밖이고 0점이 아닌 과녁이면 다음 방향 보기
 									if(nextX < 0 || nextX >= N || nextY < 0 || nextY >= N || mapTmp[nextX][nextY][0] != 0) continue;
-									else {
-										mapTmp[nextX][nextY][0] = mapTmp[i][j][1] / 4;
-										mapTmp[nextX][nextY][1] = mapTmp[nextX][nextY][0];
-									}
+									
+									// 0점인 과녁에 대해 초기점수의 1/4로 새로운 과녁 생성
+									mapTmp[nextX][nextY][0] = mapTmp[i][j][1] / 4;
+									mapTmp[nextX][nextY][1] = mapTmp[nextX][nextY][0];
 								}								
 							}
+							// 사라진 과녁의 초기값만큰 점수 갱신하고 쏘기
 							shoot(nAttack+1, score + mapTmp[i][j][1], mapTmp);
 						}else {
+							//0점이 안되면 점수 갱신 없이 쏘기
 							shoot(nAttack+1, score, mapTmp);
 						}
 					}
+					// 바뀐 값 다시 원래대로 돌리기
 					for(int k = 0; k < N; k++) {
 						for(int l = 0; l < N; l++) {
 							mapTmp[k][l][0] = map[k][l][0];
 							mapTmp[k][l][1] = map[k][l][1];
 						}
 					}
+					// 쐈으니까 다음 행 쐈을 경우 생각하기
 					continue loop;
 				}
 			}
