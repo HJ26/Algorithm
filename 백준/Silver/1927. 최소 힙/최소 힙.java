cf66b90d4d6a532dac8840a0ae49f701910e2a83
Hyunjung <57403168+HJ26@users.noreply.github.com>
2024-07-06 22:52:05 +0900

    [Silver II] Title: 최소 힙, Time: 304 ms, Memory: 26576 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		
+		PriorityQueue<Long> heap = new PriorityQueue<Long>();
+		
+		int N = Integer.parseInt(br.readLine());
+		for(int i = 0; i < N; i++) {
+			long num = Long.parseLong(br.readLine());
+			if(num != 0) {
+				heap.add(num);
+			}else {
+				if(heap.size() != 0) {
+					sb.append(heap.poll());					
+				}else {
+					sb.append("0");
+				}
+				sb.append("\n");			
+			}
+		}
+		bw.write(sb.toString());
+		bw.close();
+		br.close();
+	}
+}
