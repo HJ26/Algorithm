68409beffe0870614d6110ed612ba83414846b60
Hyunjung <57403168+HJ26@users.noreply.github.com>
2025-03-28 23:43:06 +0900

    [Silver III] Title: 파스칼, Time: 64 ms, Memory: 11484 KB -BaekjoonHub

---
+++ 백준/Silver/2986. 파스칼/파스칼.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int sqrtN = (int) Math.sqrt(N);
+        if (N%2 == 0) {
+            System.out.println(N/2);
+            return;
+        }
+        for (int i = 3; i <= sqrtN; i+=2) {
+            if (N%i == 0) {
+                System.out.println(N-N/i);
+                return;
+            }
+        }
+        System.out.println(N-1);
+    }
+}
