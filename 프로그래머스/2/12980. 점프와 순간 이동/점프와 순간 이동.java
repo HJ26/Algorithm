4ebc0a345d02c48eed44a6321db8d474b483a111
Hyunjung <57403168+HJ26@users.noreply.github.com>
2025-06-16 21:02:57 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+    static int MIN;
+    public int solution(int n) {
+        int ans = 0;
+        
+        int cur = n;
+        while(cur != 0){
+            if(cur % 2 == 0) cur /= 2;
+            else {
+                cur--;
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+}
