7b09b61e3d304ea6df1c64f727df47d7601e486d
Hyunjung <57403168+HJ26@users.noreply.github.com>
2025-08-30 23:58:05 +0900

    [Silver II] Title: 베르트랑 공준, Time: 116 ms, Memory: 12076 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class Main {
+
+	public static boolean[] prime = new boolean[246913];
+    
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+		getPrime();
+		
+		StringBuilder sb = new StringBuilder();
+ 
+		while(true) {
+			int n = Integer.parseInt(br.readLine());
+			if(n == 0) break;
+            
+			int count = 0;
+            
+			for(int i = n + 1; i <= 2 * n; i++) {
+				if(!prime[i]) count++;
+			}
+			sb.append(count).append('\n');	// 문자열로 이어준다
+		}
+		System.out.print(sb);
+	}
+	
+	public static void getPrime() {
+		prime[0] = prime[1] = true;
+		
+		for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+			if(prime[i]) continue;
+			for(int j = i * i; j < prime.length; j += i) {
+				prime[j] = true;
+			}
+		}
+	}
+ 
+}
