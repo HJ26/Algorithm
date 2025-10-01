88bf89e438bcabba932a244dd1f779c0470d1cbd
Hyunjung <57403168+HJ26@users.noreply.github.com>
2025-10-02 00:04:38 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 560 ms, Memory: 93156 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -1,117 +1,94 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
- 
-class Node implements Comparable<Node> {
-    int end;
-    int weight;
- 
-    Node(int end, int weight) {
-        this.end = end;
-        this.weight = weight;
-    }
- 
-    @Override
-    public int compareTo(Node o) {
-        return weight - o.weight;
-    }
- 
-}
- 
+import java.util.*;
+
 public class Main {
     static int N, E;
-    static ArrayList<ArrayList<Node>> a; // 인접리스트.
-    static int[] dist; // 시작점에서 각 정점으로 가는 최단거리.
-    static boolean[] check; // 방문 확인.
-    static final int INF = 200000000;
- 
-    public static void main(String[] args) throws Exception {
+    static List<Edge>[] edges;
+    static int[] vertex = new int[2];
+    static boolean[] flag = { true, true };
+
+    static class Edge{
+        int to;
+        int dist;
+
+        Edge(int to, int dist){
+            this.to = to;
+            this.dist = dist;
+        }
+
+    }
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
- 
-        a = new ArrayList<>();
-        dist = new int[N + 1];
-        check = new boolean[N + 1];
- 
-        Arrays.fill(dist, INF);
- 
-        for (int i = 0; i <= N; i++) {
-            a.add(new ArrayList<>());
+
+        edges = new ArrayList[N+1];
+        for(int i = 0; i <= N; i++){
+            edges[i] = new ArrayList<Edge>();
         }
- 
-        // 양방향 인접 리스트 구현.
-        for (int i = 0; i < E; i++) {
+
+        // 간선 정보 입력
+        for(int i = 0; i < E; i++){
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
- 
-            // start에서 end로 가는 weight (가중치)
-            a.get(start).add(new Node(end, weight));
- 
-            // end에서 start로 가는 weight (가중치)
-            a.get(end).add(new Node(start, weight));
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            edges[a].add(new Edge(b, c));
+            edges[b].add(new Edge(a, c));
         }
- 
-        // 반드시 거쳐야 하는 정점.
+
+        // 무조건 지나야 하는 정점
         st = new StringTokenizer(br.readLine());
-        int v1 = Integer.parseInt(st.nextToken());
-        int v2 = Integer.parseInt(st.nextToken());
- 
-        // 1 -> v1 -> v2 -> N으로 가는 경우
-        int res1 = 0;
-        res1 += dijkstra(1, v1);
-        res1 += dijkstra(v1, v2);
-        res1 += dijkstra(v2, N);
- 
-        // 1 -> v2 -> v1 -> N으로 가는 경우
-        int res2 = 0;
-        res2 += dijkstra(1, v2);
-        res2 += dijkstra(v2, v1);
-        res2 += dijkstra(v1, N);
- 
-        int ans = (res1 >= INF && res2 >= INF) ? -1 : Math.min(res1, res2);
- 
-        bw.write(ans + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
+        vertex[0] = Integer.parseInt(st.nextToken());
+        vertex[1] = Integer.parseInt(st.nextToken());
+
+        // 최단경로 찾기
+
+        int dist1 = djk(1,vertex[0], 0) + djk(vertex[0], vertex[1], 0) + djk(vertex[1], N, 0);
+        int dist2 = djk(1,vertex[1], 1) + djk(vertex[1], vertex[0], 1) + djk(vertex[0], N, 1);
+
+        if(!flag[0]) dist1 = Integer.MAX_VALUE;
+        if(!flag[1]) dist2 = Integer.MAX_VALUE;
+
+        int answer = Math.min(dist1, dist2);
+
+        System.out.println(answer == Integer.MAX_VALUE ? -1 : answer);
     }
- 
-    // 다익스트라 알고리즘
-    public static int dijkstra(int start, int end) {
-        Arrays.fill(dist, INF);
-        Arrays.fill(check, false);
- 
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        boolean[] check = new boolean[N + 1];
-        pq.offer(new Node(start, 0));
-        dist[start] = 0;
- 
-        while (!pq.isEmpty()) {
-            Node curNode = pq.poll();
-            int cur = curNode.end;
- 
-            if (!check[cur]) {
-                check[cur] = true;
- 
-                for (Node node : a.get(cur)) {
-                    if (!check[node.end] && dist[node.end] > dist[cur] + node.weight) {
-                        dist[node.end] = dist[cur] + node.weight;
-                        pq.add(new Node(node.end, dist[node.end]));
-                    }
-                }
+
+    private static int djk(int start, int end, int flagIdx){
+
+        if(!flag[flagIdx]) return -1;
+        if(start == end) return 0;
+        PriorityQueue<Edge> pq = new PriorityQueue<>(new Comparator<Edge>() {
+            @Override
+            public int compare(Edge o1, Edge o2) {
+                return o1.dist - o2.dist;
             }
+        });
+
+        boolean[] visit = new boolean[N+1];
+        visit[start] = true;
+        for(Edge e : edges[start]){
+            pq.add(new Edge(e.to, e.dist));
         }
- 
-        return dist[end];
+
+        while(!pq.isEmpty()){
+            Edge cur = pq.poll();
+            if(cur.to == end){
+                return cur.dist;
+            }
+            for(Edge e : edges[cur.to]){
+                if(visit[e.to]) continue;
+                pq.add(new Edge(e.to, cur.dist + e.dist));
+            }
+            visit[cur.to] = true;
+        }
+
+        flag[flagIdx] = false;
+        return -1;
     }
 }
