9ed3664e3d68b11576f84c2da2132ba8009bf8ab
Hyunjung <57403168+HJ26@users.noreply.github.com>
2026-01-18 23:33:46 +0900

    [level 2] Title: 후보키, Time: 7.27 ms, Memory: 92.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42890. 후보키/후보키.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Solution {
+	
+	static ArrayList<HashSet<Integer>> candidateKey;
+	
+	public int solution(String[][] relation){
+		candidateKey = new ArrayList<>();
+		int colSize = relation[0].length;
+		
+		for(int i = 1 ; i <= colSize ; ++i) {
+			makeKeySet(-1, colSize - 1, 0, i, new HashSet<>(), relation);
+		}
+		
+		return candidateKey.size();
+	}
+
+	private void makeKeySet(int attr, int maxAttr, int idx, int size, HashSet<Integer> keySet, String[][] relation) {
+		if(idx == size) {
+			
+			for(HashSet<Integer> key : candidateKey) {
+				if(keySet.containsAll(key)) return;
+            }
+			
+			if(isUnique(keySet, relation)) candidateKey.add(keySet);
+	
+			return;
+		}
+		
+		for(int i = attr + 1 ; i <= maxAttr ; ++i) {
+			HashSet<Integer> newKeySet = new HashSet<>(keySet);
+			newKeySet.add(i);
+			makeKeySet(i, maxAttr, idx + 1, size, newKeySet, relation);
+		}
+	}
+
+	private boolean isUnique(HashSet<Integer> keySet, String[][] relation) {
+		HashMap<String, String> map = new HashMap<>();
+		
+		for(int r = 0 ; r < relation.length ; ++r) {
+			String key = "";
+			
+			for(int c : keySet) {
+				key += relation[r][c];
+			}
+			
+			if(map.containsKey(key)) return false;
+			else map.put(key, key);
+		}	
+		return true;
+	}
+}
