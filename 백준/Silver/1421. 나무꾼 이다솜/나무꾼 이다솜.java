8e6e4b75c852e9ec201b708dde9f9c5f25cb21ed
Hyunjung <57403168+HJ26@users.noreply.github.com>
2026-03-10 22:55:39 +0900

    [Silver I] Title: 나무꾼 이다솜, Time: 84 ms, Memory: 11808 KB -BaekjoonHub

---
+++ 백준/Silver/1421. 나무꾼 이다솜/나무꾼 이다솜.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n, c, w, max;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        w = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        max = Integer.MIN_VALUE;
+
+        for(int i=0;i<n;i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=0;i<n;i++) {
+            max = Math.max(max, arr[i]);
+        }
+        System.out.print(ret(arr));
+
+    }
+
+    public static long ret(int a[]) {
+        long sum=0, maxSum=Integer.MIN_VALUE, price=0, cnt=0;
+        for(int i=1;i<=max;i++) {
+            sum=0;
+            for(int j=0;j<a.length;j++) {
+                cnt=0;
+                if(a[j]>=i) {
+                    if(a[j]%i==0)
+                        cnt = a[j]/i-1;
+                    else
+                        cnt = a[j]/i;
+                    price = ((a[j]/i)*w*i - (cnt*c));
+                    if(price > 0) sum+=price;
+                }
+            }
+            maxSum = Math.max(sum, maxSum);
+        }
+        return maxSum;
+    }
+
+}
