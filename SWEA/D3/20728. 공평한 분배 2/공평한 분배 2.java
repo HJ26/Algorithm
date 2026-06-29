b178e520cd91d2abdcb011f92ae4711780f47c41
Hyunjung <57403168+HJ26@users.noreply.github.com>
2026-06-29 22:32:11 +0900

    [D3] Title: 공평한 분배 2, Time: 99 ms, Memory: 27,520 KB -BaekjoonHub

---
+++ SWEA/D3/20728. 공평한 분배 2/공평한 분배 2.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+ 
+class Solution {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb= new StringBuilder();
+         
+        int T = Integer.parseInt(br.readLine());
+         
+        for(int i = 1; i<=T; i++) {
+             
+            st= new StringTokenizer(br.readLine(), " ");
+            int N = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+            int [] arr=  new int[N];
+            List<Integer> list = new ArrayList<Integer>();
+             
+            st = new StringTokenizer(br.readLine()," ");
+            for(int j=0; j<N; j++) {
+                arr[j]=Integer.parseInt(st.nextToken());
+            }
+             
+            Arrays.sort(arr);
+             
+            int min = Integer.MAX_VALUE;
+            for(int j=0; j<N-K+1; j++) {
+                min=Math.min(min, arr[j+K-1]-arr[j]);
+            }
+            sb.append("#").append(i+" ").append(min).append("\n");  
+        }
+        System.out.println(sb);
+ 
+    }
+ 
+}
