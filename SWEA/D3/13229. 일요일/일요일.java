806c84d7c3b8799ab1df37b2d48e1f35a84d7eb1
Hyunjung <57403168+HJ26@users.noreply.github.com>
2026-08-03 23:59:32 +0900

    [D3] Title: 일요일, Time: 87 ms, Memory: 26,880 KB -BaekjoonHub

---
+++ SWEA/D3/13229. 일요일/일요일.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+		int T = sc.nextInt();
+        String[] days = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+		for(int tc = 1; tc <= T; tc++) {
+            String day = sc.next();
+            int result = 0;
+            for(int i = 0; i < days.length; i++){
+                if(days[i].equals(day)){
+                    result = days.length - i - 1;
+                }
+            }
+            System.out.println("#" + tc + " " + (result == 0?7 : result));
+		}
+	}
+}
