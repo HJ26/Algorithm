d24eefc7460691e7512f984c1622726cfcb4d009
Hyunjung <57403168+HJ26@users.noreply.github.com>
2026-09-04 23:49:53 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 81 ms, Memory: 25,728 KB -BaekjoonHub

---
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+	static int N, B, ans;
+	static int[] height;
+	static boolean[] isSelected;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc<=T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			B = Integer.parseInt(st.nextToken());
+			
+			height = new int[N];
+			isSelected = new boolean[N];
+			
+			st = new StringTokenizer(br.readLine());
+			for (int i=0; i<N; i++) {
+				height[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			ans = Integer.MAX_VALUE;
+			subset(0, 0);
+			System.out.println("#"+tc+" "+ans);
+		}
+	}
+	
+	static void subset(int cnt, int sum) {
+		if (sum>=B) ans = Math.min(ans, sum-B);
+			
+		if (cnt == N) {
+			return;
+		}
+		
+		isSelected[cnt] = true;
+		subset(cnt+1, sum + height[cnt]);
+		isSelected[cnt] = false;
+		subset(cnt+1, sum);
+	}
+}
